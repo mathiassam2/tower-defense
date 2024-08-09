7b9d31e9eeaefe31c23b337ca7c3038f88633c8a
Mathias Sam <mathiassam2@gmail.com>
2024-08-09 12:05:22 +0800

    enemy able to rotate same direction as path

--- enemy.py
+++ enemy.py
@@ -1,5 +1,6 @@
 import pygame as pg
 from pygame.math import Vector2
+import math
 
 class Enemy(pg.sprite.Sprite):
     def __init__(self, waypoints, image):
@@ -8,12 +9,15 @@ class Enemy(pg.sprite.Sprite):
         self.pos = Vector2(self.waypoints[0])
         self.target_waypoint = 1
         self.speed = 2
-        self.image = image
+        self.angle = 0
+        self.original_image = image
+        self.image = pg.transform.rotate(self.original_image, self.angle)
         self.rect = self.image.get_rect()
         self.rect.center = self.pos
 
     def update(self):
         self.move()
+        self.rotate()
 
     def move(self):
         #define a target waypoint
@@ -32,4 +36,13 @@ class Enemy(pg.sprite.Sprite):
             if dist != 0:
                 self.pos += self.movement.normalize() * dist
             self.target_waypoint += 1
+
+    def rotate(self):
+        #calculate distance to next waypoint
+        dist = self.target - self.pos
+        #use distance to calculate angle
+        self.angle = math.degrees(math.atan2(-dist[1], dist[0]))
+        #rotate image and update rectangle
+        self.image = pg.transform.rotate(self.original_image, self.angle)
+        self.rect = self.image.get_rect()
         self.rect.center = self.pos
