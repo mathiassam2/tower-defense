850d1fca8f31cf29162169af682b7a641e4a255d
Mathias Sam <mathiassam2@gmail.com>
2024-08-09 11:59:33 +0800

    enemy able to follow path

--- enemy.py
+++ enemy.py
@@ -17,8 +17,19 @@ class Enemy(pg.sprite.Sprite):
 
     def move(self):
         #define a target waypoint
-        self.target = Vector2(self.waypoints[self.target_waypoint])
-        self.movement = self.target - self.pos
-        self.rect.x += 1
-        self.pos += self.movement.normalize() * self.speed
+        if self.target_waypoint < len(self.waypoints):
+            self.target = Vector2(self.waypoints[self.target_waypoint])
+            self.movement = self.target - self.pos
+        else:
+            #enemy has reached the end of the path
+            self.kill()
+        #calculate distance to target
+        dist = self.movement.length()
+        #check if remaining distance is greater than enemy speed
+        if dist >= self.speed:
+            self.pos += self.movement.normalize() * self.speed
+        else:
+            if dist != 0:
+                self.pos += self.movement.normalize() * dist
+            self.target_waypoint += 1
         self.rect.center = self.pos
